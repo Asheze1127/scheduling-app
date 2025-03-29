a7b9e29477bfcee3ac605b60f6a33f9386cfdef5
Asheze1127 <m.k.h11272005@gmail.com>
2025-03-30 02:47:04 +0900

    カレンダーを追加しました

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import jaLocale from '@fullcalendar/core/locales/ja';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="calendar">
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
+        initialView="dayGridMonth"
+        locales={[jaLocale]}
+        locale='ja'
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek listWeek',
+        }}
+        events={[
+          {title:'eventを', start: '2025-03-14'},
+          {title:'こんな感じで追加できます', start: '2025-03-15T10:30:00', end: '2025-03-17T14:30:00'},
+          {title:'こんな感じで追加できます', start: '2025-03-14T14:30:00', end: '2025-03-17T14:30:00'}
+        ]}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
